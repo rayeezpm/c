31f611196f458c4f98e934f9df2eddd4e2142086
Rayeez PM <rayeez.pm.new@gmail.com>
2024-06-20 15:01:19 +0530

    first commit

---
+++ test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int a=15;
+    double ff=67.456346;
+    char b[]="Hi";
+    printf("%d\n",a);
+    printf("%lf\n",ff);
+    printf("%s\n",b);
+    return 0;
+}
